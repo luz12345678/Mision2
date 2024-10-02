b4fe4b20242526adee8818e003988544dde28126
luz12345678 <gomezoluz981@gmail.com>
2024-10-02 07:52:59 -0500

    primer commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var nombre = 'jose ';
+var apellido= 'gomeez';
+
+console.log(nombre)
+
+const PI=3.14;
+console.log(PI)
+
+let persona= {
+    nombre: "luz",
+    apellido: "gomez",
+}
+
+console.log(persona);
+
+function saludar(nombre) {
+    return
+}
