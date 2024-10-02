a940cd81b7440fc32000efc9f85eeef5e4ec078e
luz12345678 <gomezoluz981@gmail.com>
2024-10-02 09:00:24 -0500

    segundo commit

--- js/app.js
+++ js/app.js
@@ -14,5 +14,31 @@ let persona= {
 console.log(persona);
 
 function saludar(nombre) {
-    return
+    return "Te amooooo, " + nombre
 }
+let saludo= saludar("Jose Luis");
+console.log(saludo); // "hola, Jose Luis"
+
+function sumar(x, y){
+    return  x+ y ;
+}
+
+console.log(sumar(3,9))
+
+function restar(x,y ) {
+    return x-y;
+}
+console.log( restar(6,3))
+
+function dividir(x,y){
+    return x/y;
+}
+console.log(dividir(12,2))
+
+function multiplicar(x,y){
+    return x*y;
+}
+console.log(multiplicar(9,3))
+
+
+
